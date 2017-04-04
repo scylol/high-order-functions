bdfe380cca617148e68261dfd52c83aa7f5cab07
pham-thien <pham.thien@outlook.com>
2017-04-04 16:04:56 -0400

    returning functions drill

--- app.js
+++ app.js
@@ -46,26 +46,58 @@
 
 // filter(myNames,filteredNames);
 
-const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 
 
 
 
-function filter(arr, fn) {
-    let newArray = [];
-    for(i=0;i<arr.length;i++){
-            const ele = arr[i];
-            if(fn(ele)) {
-            newArray.push(ele);
-            }
-    }
+// function filter(arr, fn) {
+//     let newArray = [];
+//     for(i=0;i<arr.length;i++){
+//             const ele = arr[i];
+//             if(fn(ele)) {
+//             newArray.push(ele);
+//             }
+//     }
 
     
-    return newArray;
+//     return newArray;
+// }
+
+
+//rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+function hazardWarningCreator(typeOfWarning) {
+	let warningCounter = 0;
+	return function(location) {
+		warningCounter++;
+		console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+		console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+	};
+
 }
 
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const iceWarning = hazardWarningCreator('Ice on the Road');
+const sheepWarning = hazardWarningCreator('Sheep on the Road!');
 
-console.log(filter(myNames, name => name[0] === 'R'));
+rocksWarning('Main St and Pacific Ave');
+iceWarning('37th and 8th');
+sheepWarning('5th Mulberry St');
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
