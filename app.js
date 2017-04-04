3b694f79a510f326947d62e467cdacbe73b3e371
Scylol <djd3dobbin@gmail.com>
2017-04-04 15:44:42 -0400

    filter function

--- app.js
+++ app.js
@@ -1,16 +1,71 @@
-function repeat(fn, n) {
-	for (let i = 0; i < n; i++) {
-		fn();
-	}
+// Repeat functions
+
+// function repeat(fn, n) {
+// 	for (let i = 0; i < n; i++) {
+// 		fn();
+// 	}
+// }
+
+// function hello() {
+// 	console.log('Hello world');
+// }
+
+// function goodbye() {
+// 	console.log('Goodbye world');
+// }
+
+// repeat(hello, 5);
+// repeat(goodbye, 5);
+
+//--------------------------------------------------------------------------------
+
+
+// Return only names that begin with 'R'
+// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+// const filteredNames = filter(myNames, function(name){
+//     // This is a "predicate function" - it's a function that only returns a boolean
+//     return name[0] === 'R';
+// });
+
+// console.log(filteredNames) // => ['Rich', 'Ray']
+
+// function filter(arr, fn) {
+// 	let newArray = [];
+// 	for(i=0;i<arr.length;i++){
+// 			const ele = arr[i];
+// 			if(fn(ele)) {
+// 			newArray.push(ele);
+// 			}
+// 	}
+
+	
+// 	return newArray;
+// }
+
+
+// filter(myNames,filteredNames);
+
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+
+
+
+function filter(arr, fn) {
+    let newArray = [];
+    for(i=0;i<arr.length;i++){
+            const ele = arr[i];
+            if(fn(ele)) {
+            newArray.push(ele);
+            }
+    }
+
+    
+    return newArray;
 }
 
-function hello() {
-	console.log('Hello world');
-}
 
-function goodbye() {
-	console.log('Goodbye world');
-}
+console.log(filter(myNames, name => name[0] === 'R'));
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+
+
